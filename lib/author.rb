a4d87440c6433dc3d76d5eef686a6f989a07736a
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-10 17:29:03 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,18 +1,9 @@
 class Author
 
-attr_accessor :name
+  attr_accessor :name
 
-def initialize(name)
-  @name = name
-end
-
-def add_author(author)
-  post.author = self
-end
-
-def posts
-  Post.all.select {|post| post.author == self}
-end
-
+  def initialize(name)
+    @name = name
+  end
 
 end
