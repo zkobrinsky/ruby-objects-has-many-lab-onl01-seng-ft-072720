683f9980b8bb795b1fbe47bacde582b962904590
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-10 18:14:55 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,27 @@
+class Author
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def add_post(title)
+    title.author = self
+  end
+
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+
+  def add_post_by_title(name)
+    post = Post.new(name)
+    post.author = self
+  end
+
+  def self.post_count
+    Post.all.count
+  end
+
+
+end
---
+++ lib/post.rb
@@ -0,0 +1,31 @@
+require "pry"
+
+class Post
+
+attr_accessor :author, :title #author only to access Author class
+
+@@all = []
+
+def initialize(title)
+  @title = title
+  save
+end
+
+def save
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+def author_name
+  # binding.pry
+  if self.author == nil
+    self.author
+  else
+    self.author.name
+  end
+end
+
+end
